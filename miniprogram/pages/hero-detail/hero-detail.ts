09f4e1f89b5301c4d32c0a3fb29e0ecd48b13210
熊雄 <312472644@qq.com>
2021-11-12 19:04:56 +0800

    feat: 新增技能相亲接口联调

--- miniprogram/pages/hero-detail/hero-detail.ts
+++ miniprogram/pages/hero-detail/hero-detail.ts
@@ -7,23 +7,21 @@ Page({
      * 页面的初始数据
      */
     data: {
-        heroes: null,
-        load: false
+        heroes: null
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad(option: any) {
-        this.getHeroDetail(option.id);
         if (!option) {
             return;
         }
-        // wx.setNavigationBarTitle({ title: option.name })
-        // this.getHeroDetail(option.id);
+        wx.setNavigationBarTitle({ title: option.name });
+        this.getHeroDetail(option.id);
     },
     // 获取英雄技能详情
-    getHeroDetail(heroId: number = 103) {
+    getHeroDetail(heroId: number) {
         wx.showLoading({ title: '加载中...' });
         wx.request({
             url: `https://www.dota2.com.cn/datafeed/hero?hero_id=${heroId}`,
@@ -33,24 +31,23 @@ Page({
                 if (statusCode === 200) {
                     const { result } = data;
                     this.setData({ heroes: result.heroes });
-                    console.log('res', result.heroes);
                 } else {
                     wx.showToast({
                         title: "获取数据失败",
                         icon: "error"
                     });
                 }
-                this.setData({ load: true });
             },
             complete() {
                 wx.hideLoading();
             }
         })
     },
+    // 英雄预览
     perviewHero(event: IEvent) {
         const { detail } = event;
         const heroId = detail.heroId;
-        this.setData({ load: false });
+        this.setData({ heroes: null });
         this.getHeroDetail(heroId);
     }
 })
