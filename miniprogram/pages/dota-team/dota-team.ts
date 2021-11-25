38bb23b83e444c42c77547ec043d87c10015470a
312472644 <312472644@qq.com>
2021-11-25 20:04:48 +0800

    feat: 新增职业战队页面

--- miniprogram/pages/dota-team/dota-team.ts
+++ miniprogram/pages/dota-team/dota-team.ts
@@ -1,3 +1,5 @@
+import { IEvent, IResult } from "miniprogram/interface";
+
 // pages/dota-team/dota-team.ts
 Page({
 
@@ -5,14 +7,14 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        teamList: []
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad() {
-
+        this.getTeamList();
     },
 
     /**
@@ -20,5 +22,21 @@ Page({
      */
     onReady() {
 
+    },
+    getTeamList() {
+        wx.showLoading({ title: '加载中...' });
+        wx.request({
+            url: 'https://api.opendota.com/api/teams',
+            method: 'GET',
+            success: (res: IResult<any>) => {
+                const { data = [], statusCode } = res;
+                if (statusCode === 200) {
+                    this.setData({ teamList: (data as any).filter((item: any) => item.name && item.logo_url).splice(0, 100) })
+                }
+            },
+            complete: () => {
+                wx.hideLoading();
+            }
+        })
     }
 })
